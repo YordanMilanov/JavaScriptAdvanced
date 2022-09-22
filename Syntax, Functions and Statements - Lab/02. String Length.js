a5fe9ed08f30b0e572180602687adf1112eb3197
YordanMilanov <dancho.galinov@gmail.com>
2022-09-22 08:55:56 +0300

    task 2

---
+++ Syntax, Functions and Statements - Lab/02. String Length.js
@@ -0,0 +1,11 @@
+function calcLength (x, y, z) {
+    let totalLength = x.length + y.length + z.length;
+    let averageLength = Math.floor(totalLength / 3);
+    
+    console.log(calcLength)
+    console.log(averageLength)
+}
+
+
+
+
