8891516bccc00e651142f7ad2b28626c7f4b247d
YordanMilanov <dancho.galinov@gmail.com>
2022-10-20 15:33:41 +0300

    not working task 2

--- Nested Arrays - Lab/02LastKNumbersSquence.js
+++ Nested Arrays - Lab/02LastKNumbersSquence.js
@@ -1,3 +1,39 @@
-function lastK() {
-    
-}
+//n - length
+//k - count of last numbers we want to add
+
+function lastK(n, k) {
+    let repeats = k;
+    let currentSum = 0;
+    let arrayToCreate = [1];
+
+    if (arrayToCreate.length < repeats) {
+
+        for (let i = 0; i < arrayToCreate.length; i++) {
+
+            for (let j = i; j > -repeats - i; j--) {
+                if (j >= 0) {
+                    currentSum += arrayToCreate[j];
+                }
+            };
+
+            arrayToCreate[i + 1] = currentSum;
+            currentSum = 0;
+            console.log(arrayToCreate);
+
+            if (arrayToCreate.length < repeats) {
+                break;
+            };
+        };
+
+    } else {
+        for (let i = arrayToCreate.length - repeats; i < arrayToCreate.length; i++) {
+            currentSum += arrayToCreate[i];
+        };
+        arrayToCreate[i + 1] = currentSum;
+        currentSum = 0;
+        console.log(arrayToCreate);
+    };
+
+};
+
+lastK(6, 3);
