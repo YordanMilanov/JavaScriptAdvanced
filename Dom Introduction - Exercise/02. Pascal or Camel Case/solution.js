957fb887c7ba61c893fd56e85755b1a962abbbfe
YordanMilanov <dancho.galinov@gmail.com>
2022-12-28 10:52:56 +0200

    added task 2

--- Dom Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ Dom Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let style = document.getElementById('naming-convention').value;
+  let result = '';
+  let words = text.split(/(\s+)/).filter(x => x != ' ');
+
+  if(style == "Camel Case") {
+    for (let index = 0; index < words.length; index++) {
+      words[index] = words[index].toLowerCase();
+      if(index != 0) {
+        words[index] = words[index].charAt(0).toUpperCase() + words[index].substring(1);
+      }
+    }
+  } else if (style == "Pascal Case") {
+    for (let index = 0; index < words.length; index++) {
+    words[index] = words[index].toLowerCase();
+    words[index] = words[index].charAt(0).toUpperCase() + words[index].substring(1);
+    }
+  } else {
+    return document.getElementById('result').textContent = 'Error!';
+  }
+  result = words.join('');
+  document.getElementById('result').textContent = result;
 }
