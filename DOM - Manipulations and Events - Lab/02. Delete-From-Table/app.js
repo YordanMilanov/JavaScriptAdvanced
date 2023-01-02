3bf5fa483c0320e066883826ea0d2e53e6dbf814
YordanMilanov <dancho.galinov@gmail.com>
2023-01-02 14:04:52 +0200

    2nd 100%

--- DOM - Manipulations and Events - Lab/02. Delete-From-Table/app.js
+++ DOM - Manipulations and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,27 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    //select input field and read value
+     const value = document.querySelector('input[name="email"]').value;
+     console.log('deleting', value)
+
+    //repeat for every table row
+    const rows = document.querySelectorAll('tbody tr');
+
+    // -- select second column
+    // -- compare text content with input value
+    // -- if they are equal - remove current row
+    let found = false;
+    for(let row of rows) {
+        if(row.children[1].textContent == value) {
+            const parent = row.parentElement;
+            parent.removeChild(row);
+            found = true;
+        }
+    }
+
+    // display result
+    if(found){
+        document.getElementById('result').textContent = "Deleted.";
+    } else {
+        document.getElementById('result').textContent = "Not found.";
+    }
 }
