be6140dc9017c8405ef22169f6c9e6cec613c563
YordanMilanov <dancho.galinov@gmail.com>
2023-01-02 17:17:49 +0200

    3rd 100%

--- DOM - Manipulations and Events - Lab/03. Add-Delete/app.js
+++ DOM - Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,34 @@
 function addItem() {
-    //TODO...
+    //select input field and read its value
+    const content = document.getElementById('newItemText').value;
+    
+    //create <li> element
+    const liElement = document.createElement('li');
+    //assign input value to element text content
+    liElement.textContent = content;
+     
+    //create [Delete] anchor
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#'; 
+    liElement.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onDelete);
+
+    //select <ul> and append the new element
+    const ulElement = document.getElementById(`items`);
+    ulElement.appendChild(liElement);
+
+    //select the input field and clear the contents (value)
+    document.getElementById('newItemText').value = '';
+
+    function onDelete(event) {
+        //event <- свързва нли с евента, който се е случил на атрибута на елемента,
+        //тоест натискането на бутона [delete]
+        //target <- дава ни [delete] button-a като елемент.
+        //parentElement <- връща ни parent-a на target-a(delete button-a)
+        //remove() - премахва целия parent.
+        event.target.parentElement.remove();
+    }
 }
+
