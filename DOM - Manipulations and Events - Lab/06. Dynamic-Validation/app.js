76c68c1a619beabc78f5baa9ec938ee6bfb02521
YordanMilanov <dancho.galinov@gmail.com>
2023-01-03 22:20:08 +0200

    6th 100%

--- DOM - Manipulations and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM - Manipulations and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,19 @@
 function validate() {
-    console.log('TODO:...');
+    //select input field
+    //add change event listener
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange(ev) {
+        //onChange -> validate input
+       const email = ev.target.value;
+       const regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+       //regex.test(email) <- tests if the parameter matches the regex
+      if(regex.test(email)) {
+        //if valid -> remove class error
+        ev.target.className = '';
+      } else {
+        //if invalid -> add class error
+        ev.target.className = 'error';
+      }
+    }
 }
