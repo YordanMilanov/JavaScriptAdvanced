e110177417ecc8dbdfa4ad750d4fced639f5b2bd
YordanMilanov <dancho.galinov@gmail.com>
2023-01-12 15:14:03 +0200

    task 2 100

--- DOM - Manipulations and Events - Exercise/02. Time-Converter/solution.js
+++ DOM - Manipulations and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,46 @@
 function attachEventsListeners() {
+    //buttons
+    let daysBtn = document.getElementById('daysBtn');
+    let hoursBtn = document.getElementById('hoursBtn');
+    let minutesBtn = document.getElementById('minutesBtn');
+    let secondsBtn = document.getElementById('secondsBtn');
 
-    console.log('TODO:...');
+    //input fields
+    let days = document.getElementById('days');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+
+    let rations = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400,
+    };
+
+    daysBtn.addEventListener('click',onConvert);
+    hoursBtn.addEventListener('click', onConvert);
+    minutesBtn.addEventListener('click', onConvert);
+    secondsBtn.addEventListener('click', onConvert);
+
+    function convert(value, unit) {
+        //unit = единицата която са ни подали като стринг(ден,минути,секунди,часове)
+        let days = value / rations[unit];
+        return {
+            days: days,
+            hours: days * rations.hours,
+            minutes: days * rations.minutes,
+            seconds: days * rations.seconds
+        }
+    }
+
+    function onConvert(event) {
+        let input = event.target.parentElement.querySelector('input[type="text"]');
+        let time = convert(Number(input.value), input.id);
+
+        days.value = time.days;
+        hours.value = time.hours;
+        minutes.value = time.minutes;
+        seconds.value = time.seconds;
+    };
 }
