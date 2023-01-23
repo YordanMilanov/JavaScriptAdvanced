29e79f94f933ea3ed511947c22c3d68ef65cf331
YordanMilanov <dancho.galinov@gmail.com>
2023-01-23 22:00:43 +0200

    task 4 fixed

--- Advanced Functions - Exercise/robot.js
+++ Advanced Functions - Exercise/robot.js
@@ -1,4 +1,4 @@
-function solution () {
+function solution() {
     let store = {
         protein: 0,
         carbohydrate: 0,
@@ -7,11 +7,11 @@ function solution () {
     }
 
     let recipesEnum = {
-        apple: {carbohydrate: 1, flavour: 2},
-        burger:{carbohydrate: 10, fat: 7, flavour: 3},
-        lemonade: {carbohydrate: 10, flavour: 20},
-        eggs: {protein: 10, fat: 1, flavour: 1},
-        turkey: {protein: 10, carbohydrate: 10, fat: 10, flavour: 10}
+        apple: { carbohydrate: 1, flavour: 2 },
+        burger: { carbohydrate: 5, fat: 7, flavour: 3 },
+        lemonade: { carbohydrate: 10, flavour: 20 },
+        eggs: { protein: 5, fat: 1, flavour: 1 },
+        turkey: { protein: 10, carbohydrate: 10, fat: 10, flavour: 10 }
     }
 
     return function inputHandler(input) {
@@ -28,20 +28,30 @@ function solution () {
             },
             prepare: (recipe, quantity) => {
                 let isDone = true;
-                for(let [key, defQuantity] of Object.entries(recipesEnum[recipe])){
+                let str = "";
+                let copyStore = JSON.parse(JSON.stringify(store));
+                for (let [key, defQuantity] of Object.entries(recipesEnum[recipe])) {
                     let needValue = Number(quantity) * defQuantity;
-                    if(store[key] < needValue) {
+                    if (store[key] < needValue) {
                         isDone = false;
+                        str = `Error: not enough ${key} in stock`
                         break;
                     }
+                    copyStore[key] -= needValue;
                 };
-
+                if (!isDone) {
+                    return str;
+                }
+                store = copyStore;
+                return "Success";
             },
-            report: ()=> console.log('report')
+            report: () => {
+                return `protein=${store.protein} carbohydrate=${store.carbohydrate} fat=${store.fat} flavour=${store.flavour}`
+            }
         }
-    } 
+    }
 }
 
-let manager = solution();
-console.log(manager ("restock flavour 50"));
+    let manager = solution();
+    console.log(manager("restock flavour 50"));
 //console.log (manager ("prepare lemonade 4"));
