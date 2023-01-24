15bb56bfe86086812a64cee8dfb632017f67c702
YordanMilanov <dancho.galinov@gmail.com>
2023-01-24 15:28:33 +0200

    task 9 to be finshed

--- Advanced Functions - Exercise/09. Task Manager/app.js
+++ Advanced Functions - Exercise/09. Task Manager/app.js
@@ -1,3 +1,40 @@
 function solve() {
-    console.log("//TODO")
+
+    // collect field values
+    const input = {
+        name: document.getElementById('task'),
+        description: document.getElementById('description'),
+        Date: document.getElementById('date')
+    }
+
+    const [_, openSection, progressSection, finishedSection] = Array.from(document.querySelectorAll('section'));
+    document.getElementById(`add`).addEventListener('click', addTask);
+
+    function addTask(event) {
+        event.preventDefault();
+        // create elements
+        const article = document.createElement('h3');
+        article.appendChild(createElement('h3', input.name.value));
+        article.appendChild(createElement('p', `Description: ${input.description.value}`));
+        article.appendChild(createElement('p', `Due Date: ${input.Date.value}`));
+        const div = createElement('div', '', 'flex');
+        div.appendChild(createElement('button', 'Start', 'green'));
+        div.appendChild(createElement('button', 'Delete', 'red'));
+        article.appendChild(div);
+        // append to open section
+        openSection.appendChild(article);
+
+        Object.values(input).forEach(i => i.value = '');
+
+        // add functionallity tfor start,finish,delete task
+    }
+
+    function createElement(type, content, className) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        if (className) {
+            element.className = className;
+        }
+        return element;
+    }
 }
